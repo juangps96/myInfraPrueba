226ec9cbf73751d8a4681f39bf2e20b7455e7583
Juan Peñaherrera <juangps96@hotmail.com>
2023-12-26 15:11:05 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mi-s3-bucket-for-tf-state"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "mi-dynamo-db-table-tf"
   }
 }
